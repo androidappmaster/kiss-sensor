d492170922c11722d548484adddd4af81f06813d
Manolo Vera <manolo.vn@gmail.com>
2014-04-07 22:34:56 +0200

    added lips image

--- app/src/main/java/es/appmaster/kissmaster/MainActivity.java
+++ app/src/main/java/es/appmaster/kissmaster/MainActivity.java
@@ -8,16 +8,18 @@ import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private ImageView kissImage;
     private SensorManager sensorManager;
     private SensorEventListener sensorEventListener;
     private Sensor sensor;
 
-    private MediaPlayer mp;
-
+    private MediaPlayer mediaPlayer;
     static boolean isPlaying = false;
 
     @Override
@@ -25,6 +27,8 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        kissImage = (ImageView) findViewById(R.id.kissImage);
+        kissImage.setVisibility(View.INVISIBLE);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
@@ -32,39 +36,34 @@ public class MainActivity extends ActionBarActivity {
 
             @Override
             public void onSensorChanged(SensorEvent event) {
-                if ( event.values[0] <= 1 ) {
-                    if ( isPlaying )
-                        return;
+                if ( event.values[0] <= 1 && !isPlaying ) {
 
                     // play kiss sound
                     isPlaying = true;
-                    mp = MediaPlayer.create(MainActivity.this, R.raw.kiss);
-                    mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    kissImage.setVisibility(View.VISIBLE);
+                    mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.kiss);
+                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 
                         @Override
-                        public void onCompletion(MediaPlayer mp) {
-                            mp.release();
+                        public void onCompletion(MediaPlayer mediaPlayer) {
+                            mediaPlayer.release();
+                            kissImage.setVisibility(View.INVISIBLE);
                             isPlaying = false;
                         }
 
                     });
-                    mp.start();
+                    mediaPlayer.start();
                 }
             }
 
             @Override
             public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+                // not used
             }
 
         };
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
 
-        sensorManager.unregisterListener(sensorEventListener);
     }
 
     @Override
@@ -76,4 +75,12 @@ public class MainActivity extends ActionBarActivity {
                 sensor,
                 SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(sensorEventListener);
+    }
+
 }
